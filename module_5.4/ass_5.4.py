e354da062c3f8978221ce29081386f6a7edfc764
Esther-EA <estherakpanowo@gmail.com>
2022-10-11 11:27:14 +0100

    new files

--- module_5.4/ass_5.4.py
+++ module_5.4/ass_5.4.py
@@ -2,7 +2,17 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 #pass column names as argument
-column ="Names","Mathematics", "English","Civics","Physics"
+column = [
+    "Names", 
+    "Mathematics", 
+    "English", 
+    "Civics", 
+    "Physics"
+    ]
 
-data = pd.read_csv("scores_waec.csv", file = column)
+data = pd.read_csv("scores_waec.csv", names = column)
+
+engine = create_engine("postgresql+psycopg2://postgres:datafreak@localhost:5432/demo_db")
+
+data.to_sql("scores_waec", engine, index = False)
 
