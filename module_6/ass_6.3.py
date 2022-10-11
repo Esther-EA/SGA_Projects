2d53c0d0fe6626caf4b9f2f9b84ee403edc49f0a
Esther-EA <estherakpanowo@gmail.com>
2022-10-11 11:37:59 +0100

    new

--- module_6/ass_6.3.py
+++ module_6/ass_6.3.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+# Generate an array of numbers between 1 and 100 with both numbers included and find the LCM of the even numbers. 
 arr = np.arange(1, 101, 2)
 x = np.lcm.reduce(arr)
 print(x)
